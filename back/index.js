87bce7f835144c74ee9dd09d1de6dd1c22a514bf
Seongil-Shin <callmeshin75@gmail.com>
2021-06-22 01:15:45 +0900

    feat : connect db

--- back/index.js
+++ back/index.js
@@ -6,4 +6,15 @@ const server = new GraphQLServer({
    resolvers: resolvers,
 });
 
+const { sequelize } = require("./models");
+
+sequelize
+   .sync({ force: false })
+   .then(() => {
+      console.log("데이터베이스 연결 성공");
+   })
+   .catch((err) => {
+      console.error(err);
+   });
+
 server.start(() => console.log("graph Ql server running"));
