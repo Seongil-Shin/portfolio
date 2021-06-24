416ef5b8f0c8c58983c6e1f7f5a504f3b01c7454
Seongil-Shin <callmeshin75@gmail.com>
2021-06-24 21:31:56 +0900

    feat : add landing page

--- front/src/components/Landing.jsx
+++ front/src/components/Landing.jsx
@@ -1,6 +1,50 @@
-import React from "react";
+/** @jsxImportSource @emotion/react */
+import React, { useState } from "react";
+import { css } from "@emotion/react";
+import Typewriter from "typewriter-effect";
+
+const container = `
+text-align:center;
+margin-top:30vh;
+`;
 
 function Landing() {
-   return <div>처음</div>;
+   const [startSecond, setStartSecond] = useState(false);
+
+   return (
+      <div
+         css={css`
+            ${container}
+         `}>
+         <Typewriter
+            onInit={(typewriter) => {
+               typewriter
+                  .typeString("<h1>스스로 성장하는 개발자가 되자.</h1>")
+                  .callFunction(() => {
+                     setStartSecond(true);
+                  })
+                  .stop()
+                  .start();
+            }}
+            options={{ cursor: "" }}
+         />
+         {startSecond && (
+            <Typewriter
+               onInit={(typewriter) => {
+                  if (startSecond) {
+                     typewriter
+                        .pauseFor(500)
+                        .typeString(
+                           "<h1>안녕하세요. 프론트엔드 개발자 신성일입니다.</h1>"
+                        )
+                        .stop()
+                        .start();
+                  }
+               }}
+               options={{ cursor: "" }}
+            />
+         )}
+      </div>
+   );
 }
 export default Landing;
