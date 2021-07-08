b276777be3de46f003e8535e00303c299f01f344
Seongil-Shin <callmeshin75@gmail.com>
2021-07-08 17:06:57 +0900

    style : some change on landing page

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,5 +1,5 @@
 /** @jsxImportSource @emotion/react */
-import React, { useState } from "react";
+import React from "react";
 import { css } from "@emotion/react";
 import Typewriter from "typewriter-effect";
 import mediaquery from "../lib/styles/mediaquery";
@@ -13,39 +13,23 @@ const container = `
 `;
 
 function Landing({ strings }) {
-   const [startSecond, setStartSecond] = useState(false);
-
    return (
       <div
          css={css`
             ${container}
          `}>
-         {strings[0] && (
+         {strings[0] && strings[1] && (
             <Typewriter
                onInit={(typewriter) => {
                   typewriter
                      .typeString(`<h1>${strings[0]}</h1>`)
-                     .callFunction(() => {
-                        setStartSecond(true);
-                     })
+                     .pauseFor(50)
+                     .typeString(`<br/>`)
+                     .typeString(`<h1>${strings[1]}</h1>`)
                      .stop()
                      .start();
                }}
-               options={{ cursor: "" }}
-            />
-         )}
-         {startSecond && (
-            <Typewriter
-               onInit={(typewriter) => {
-                  if (startSecond) {
-                     typewriter
-                        .pauseFor(50)
-                        .typeString(`<h1>${strings[1]}</h1>`)
-                        .stop()
-                        .start();
-                  }
-               }}
-               options={{ cursor: "" }}
+               options={{ cursor: "", delay: 70 }}
             />
          )}
       </div>
--- src/containers/LandingContainer.jsx
+++ src/containers/LandingContainer.jsx
@@ -5,13 +5,27 @@ import Landing from "../components/Landing";
 import { getLandingStrings } from "../lib/api/landing";
 
 function LandingContainer() {
-   const [strings, setStrings] = useState(["", ""]);
+   const [strings, setStrings] = useState([]);
 
    useEffect(() => {
       const getData = async () => {
          await getLandingStrings()
             .then((res) => {
-               setStrings(res.data.listLandings.items[0].lines);
+               //글자 크기는 32px, 띄어쓰기는 4px. 단어가 잘라질거 같으면 띄어쓰기 추가
+               res.data.listLandings.items[0].lines.forEach((item) => {
+                  let line = item.split(" ");
+                  let winWidth = window.innerWidth;
+                  let idx = 0;
+                  for (let i of line) {
+                     winWidth -= 32 * i.length + 4;
+                     if (winWidth < 0) {
+                        line[idx] = "<br />" + line[idx];
+                        winWidth = window.innerWidth;
+                     }
+                     idx++;
+                  }
+                  setStrings((prev) => [...prev, line.join(" ")]);
+               });
             })
             .catch((err) => {
                console.log(err);
@@ -20,6 +34,7 @@ function LandingContainer() {
       getData();
    }, []);
 
+   console.log(strings);
    return <Landing strings={strings} />;
 }
 export default LandingContainer;
